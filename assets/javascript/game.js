e34e38880fed8d7ca6563bf1d4200e66c7a53e67
IzzleNizzle <pehisaac@gmail.com>
2018-02-05 18:09:05 -0700

    Made some changes to the logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var wins1 = document.getElementById("wins");
 var losses = document.getElementById("losses");
 var guessRemaining = document.getElementById("guess-remaining");
 var lettersGuessed = document.getElementById("letters-guessed");
+var currentWord = document.getElementById("current-word");
 var keyClicked;
 var compChoice;
 var wins = "0";
@@ -11,6 +12,11 @@ var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var guessed = [];
 var keyClickedHistory = [];
 var keyGuessed = false;
+var skateboardTricks = ["ollie", "kickflip", "popshuvit", "varial", "boardslide"];
+var guessingWord = [];
+var dashWord = "";
+var keyMatch = false;
+var emptyVar = [];
 
 
 // Get game started, show scores at 0 and clear any array data.
@@ -46,18 +52,33 @@ document.onkeyup = function() {
         // Record letter into history so as to avoid duplicate letters
         keyClickedHistory.push(a);
         guessed.push(a);
+        console.log(a);
+        // Check to see if said key matches any of the letters
+        keyMatch = false;
+        for (i=0; i < compChoice.length; i++) {
+            if (a === guessingWord[i]) {
+                keyMatch = true;
+                emptyVar[i] = a;
+                // console.log("you guessed a right letter");
+            }
+        }
 
-        // Logic for if the user choice matches the randomly chosen letter
-        if (a === compChoice) {
-            wins++
-            roundReset();
-        } else {
+        if (keyMatch === false) {
             guessesRemaining--;
         }
-        // Catch for if they run out of guesses.
+        // // Logic for if the user choice matches the randomly chosen letter
+        // if (a === compChoice) {
+        //     wins++
+        //     roundReset();
+        // } else {
+        //     guessesRemaining--;
+        // }
+        // // Catch for if they run out of guesses.
         if (guessesRemaining === 0) {
             lossCount++;
+            emptyVar = [];
             roundReset();
+            
         }
         
         // Print latest scores and guesses remaining.
@@ -65,6 +86,7 @@ document.onkeyup = function() {
         wins1.textContent = wins;
         losses.textContent = lossCount;
         lettersGuessed.textContent = guessed;
+        currentWord.textContent = emptyVar;
 
   }
 
@@ -72,8 +94,25 @@ document.onkeyup = function() {
     //Resets all arrays storing user data and prints out current scores at 0
     guessed = [];
     keyClickedHistory = [];
-    compChoice = alpha[Math.floor((Math.random() * alpha.length))];
-    guessesRemaining = 9;
+    compChoice = skateboardTricks[Math.floor((Math.random() * skateboardTricks.length))];
+    console.log(compChoice);
+    console.log(skateboardTricks);
+    console.log(compChoice.length);
+    // giving dashWord dashes for every letter of the word to be displayed and guessed against
+    for (i = 0; i < compChoice.length; i++) {
+
+        // dashWord = dashWord + compChoice.charAt(i);
+        guessingWord.push(compChoice.charAt(i));
+        console.log(guessingWord);
+    }
+    // Print to current-word contents of guessingWord array without comma's
+    for (i=0; i < compChoice.length; i++) {
+        dashWord = dashWord + " -" ;
+        emptyVar.push("-");
+    }
+    console.log(guessingWord);
+    currentWord.textContent = emptyVar;
+    guessesRemaining = 12;
     guessRemaining.textContent = guessesRemaining;
     wins1.textContent = wins;
     losses.textContent = lossCount;
